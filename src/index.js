341673f3748eda9797e256260b87178dfe814dfd
Anka <103377387+ann153476@users.noreply.github.com>
2023-02-16 00:46:47 +0200

    new

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  rootElement
 );
+//////////////МОЕ///////////////
+let color = true;
+const body = document.querySelector("body");
+const btn = document.querySelector(".switch");
+function f (){
+  if(color){
+    body.style.backgroundColor="red";
+    color=false;
+  }else{
+    body.style.backgroundColor="blue";
+    color=true
+  }
+  
+}
+btn.addEventListener('click',f)
